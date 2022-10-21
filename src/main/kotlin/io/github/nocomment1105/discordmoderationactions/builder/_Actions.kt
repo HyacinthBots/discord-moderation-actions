94890d5d5ad1fdf8f4ad203622cfea41d461f4b7
NoComment1105 <nocomment1105@outlook.com>
2022-10-21 13:47:55 +0100

    Move action functions into separate file

---
+++ src/main/kotlin/io/github/nocomment1105/discordmoderationactions/builder/_Actions.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2022 NoComment1105 <nocomment1105@outlook.com>
+ *
+ * This file is part of discord-moderation-actions.
+ *
+ * Licensed under the MIT license. For more information,
+ * please see the LICENSE file or https://mit-license.org/
+ */
+
+package io.github.nocomment1105.discordmoderationactions.builder
+
+import com.kotlindiscord.kord.extensions.commands.application.slash.SlashCommandContext
+import com.kotlindiscord.kord.extensions.utils.dm
+import com.kotlindiscord.kord.extensions.utils.timeoutUntil
+import com.kotlindiscord.kord.extensions.utils.toDuration
+import dev.kord.core.behavior.ban
+import dev.kord.core.behavior.channel.createEmbed
+import dev.kord.core.behavior.edit
+import dev.kord.core.behavior.getChannelOfOrNull
+import dev.kord.core.entity.channel.GuildMessageChannel
+import io.github.nocomment1105.discordmoderationactions.builder.action.BanActionBuilder
+import io.github.nocomment1105.discordmoderationactions.enums.DMOutcome
+import kotlinx.datetime.TimeZone
+
+/**
+ * DSL function for easily running a ban action.
+ * This will ban the user, and carryout any extra tasks specified
+ *
+ * @param builder - Builder lambda used for setting up the ban action
+ * @see BanActionBuilder
+ */
+public suspend inline fun SlashCommandContext<*, *>.ban(builder: BanActionBuilder.() -> Unit) {
+	val action = BanActionBuilder()
+	action.builder()
+
+	if (action.sendDm) {
+		val dm = action.user.asUser().dm {
+			action.dmEmbedBuilder
+		}
+		if (dm == null) {
+			action.dmOutcome = DMOutcome.FAIL.message()
+		} else {
+			action.dmOutcome = DMOutcome.SUCCESS.message()
+		}
+	}
+
+	if (action.removeTimeout) {
+		action.user.asMemberOrNull(guild!!.id)?.edit { timeoutUntil = null }
+	}
+
+	action.user.asMember(guild!!.id).ban {
+		reason = action.reason
+		deleteMessageDuration = action.deleteMessageDuration.toDuration(TimeZone.UTC)
+	}
+
+	if (action.logPublicly != null && action.logPublicly == true) {
+		channel.createEmbed {
+			action.publicActionEmbedBuilder
+		}
+	}
+
+	if (!action.sendActionLog && (action.hasLogChannelPerms == null || action.hasLogChannelPerms == true)) {
+		guild!!.getChannelOfOrNull<GuildMessageChannel>(action.loggingChannel.id)?.createEmbed {
+			action.actionEmbedBuilder
+		}
+	}
+}
--- src/main/kotlin/io/github/nocomment1105/discordmoderationactions/builder/action/BanActionBuilder.kt
+++ src/main/kotlin/io/github/nocomment1105/discordmoderationactions/builder/action/BanActionBuilder.kt
@@ -9,21 +9,11 @@
 
 package io.github.nocomment1105.discordmoderationactions.builder.action
 
-import com.kotlindiscord.kord.extensions.commands.application.slash.SlashCommandContext
-import com.kotlindiscord.kord.extensions.utils.dm
-import com.kotlindiscord.kord.extensions.utils.timeoutUntil
-import com.kotlindiscord.kord.extensions.utils.toDuration
-import dev.kord.core.behavior.ban
-import dev.kord.core.behavior.channel.createEmbed
-import dev.kord.core.behavior.edit
-import dev.kord.core.behavior.getChannelOfOrNull
 import dev.kord.core.entity.User
 import dev.kord.core.entity.channel.GuildMessageChannel
 import dev.kord.rest.builder.message.EmbedBuilder
 import io.github.nocomment1105.discordmoderationactions.annotations.ActionBuilderDSL
-import io.github.nocomment1105.discordmoderationactions.enums.DMOutcome
 import kotlinx.datetime.DateTimePeriod
-import kotlinx.datetime.TimeZone
 
 @ActionBuilderDSL
 public open class BanActionBuilder : Action {
@@ -94,49 +84,5 @@ public open class BanActionBuilder : Action {
 	 */
 	public override fun publicActionEmbed(builder: EmbedBuilder.() -> Unit) {
 		builder(publicActionEmbedBuilder)
-	}
-}
-
-/**
- * DSL function for easily running a ban action.
- * This will ban the user, and carryout any extra tasks specified
- *
- * @param builder - Builder lambda used for setting up the ban action
- * @see BanActionBuilder
- */
-public suspend inline fun SlashCommandContext<*, *>.ban(builder: BanActionBuilder.() -> Unit) {
-	val action = BanActionBuilder()
-	action.builder()
-
-	if (action.sendDm) {
-		val dm = action.user.asUser().dm {
-			action.dmEmbedBuilder
-		}
-		if (dm == null) {
-			action.dmOutcome = DMOutcome.FAIL.message()
-		} else {
-			action.dmOutcome = DMOutcome.SUCCESS.message()
-		}
-	}
-
-	if (action.removeTimeout) {
-		action.user.asMemberOrNull(guild!!.id)?.edit { timeoutUntil = null }
-	}
-
-	action.user.asMember(guild!!.id).ban {
-		reason = action.reason
-		deleteMessageDuration = action.deleteMessageDuration.toDuration(TimeZone.UTC)
-	}
-
-	if (action.logPublicly != null && action.logPublicly == true) {
-		channel.createEmbed {
-			action.publicActionEmbedBuilder
-		}
-	}
-
-	if (!action.sendActionLog && (action.hasLogChannelPerms == null || action.hasLogChannelPerms == true)) {
-		guild!!.getChannelOfOrNull<GuildMessageChannel>(action.loggingChannel.id)?.createEmbed {
-			action.actionEmbedBuilder
-		}
 	}
 }
